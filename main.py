079ab55c524275085684cc9c94e591636dd50346
Simon Rost <rost.simon@gmail.com>
2025-05-20 23:12:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import requests
+import time
+from send_telegram_message import send_telegram_message
+from dotenv import load_dotenv
+
+STOCK_NAME = "NVDA"
+NOTICEABLE_DIFFERENCE = 3
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+load_dotenv()
+api_alpha = os.getenv("api_alpha")
+api_news = os.getenv("api_news")
+
+def get_stockprice():
+    params_pricecall = {
+        "function": "TIME_SERIES_DAILY",
+        "symbol": "NVDA",
+        "outputsize": "compact",
+        "datatype": "json",
+        "apikey": api_alpha,
+    }
+
+    response = requests.get(STOCK_ENDPOINT, params=params_pricecall)
+    data = response.json()
+
+    last_weekday = list(data["Time Series (Daily)"])[0]
+    lw_closing = float(data["Time Series (Daily)"][last_weekday]["4. close"])
+
+    day_before_last_weekday = list(data["Time Series (Daily)"])[1]
+    db_lw_closing = float(data["Time Series (Daily)"][day_before_last_weekday]["4. close"])
+
+    closing_price_diff_perc = round(((lw_closing - db_lw_closing) / db_lw_closing) * 100, 2)
+
+    if abs(closing_price_diff_perc) > NOTICEABLE_DIFFERENCE:
+        return closing_price_diff_perc
+    else:
+        return False
+
+def get_news(input_text: int):
+    params_newscall = {
+        "apiKey": api_news,
+        "q": "NVIDIA",
+    }
+
+    response_news = requests.get(NEWS_ENDPOINT, params=params_newscall)
+    data_news = response_news.json()
+    news_top_three = data_news["articles"][0:3]
+
+    diff_stockprice = input_text
+    if diff_stockprice > 0:
+        print_diff = f"{STOCK_NAME}: 🔺{abs(diff_stockprice)}"
+    else:
+        print_diff = f"{STOCK_NAME}: 🔻{abs(diff_stockprice)}"
+
+    news_top_three_clean = [(news["title"], news["description"], news["url"]) for news in news_top_three]
+
+    send_telegram_message(print_diff)
+    time.sleep(2)
+    for title, description, url in news_top_three_clean:
+        message = (f"{title}: {description}\n"
+                   f"{url}")
+        send_telegram_message(message)
+        time.sleep(2)
+
+run_analysis = get_stockprice()
+
+if run_analysis:
+    get_news(run_analysis)
+else:
+    pass
---
+++ send_telegram_message.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+import requests
+import time
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_telegram_message(message: str):
+    if not BOT_TOKEN or not CHAT_ID:
+        print("Missing BOT_TOKEN or CHAT_ID in environment.")
+        return
+
+    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    payload = {'chat_id': CHAT_ID, 'text': message}
+    retries = 3
+
+    for attempt in range(retries):
+        try:
+            response = requests.post(telegram_url, data=payload, timeout=5)
+            response.raise_for_status()  # Raises an error for bad HTTP responses
+            print("Message sent successfully.")
+            return
+        except Exception as e:
+            print(f"Error: {e}")
+            if attempt < retries - 1:
+                print("Retrying...")
+                time.sleep(2 ** attempt)  # Exponential backoff
+            else:
+                print("Failed after retries.")
